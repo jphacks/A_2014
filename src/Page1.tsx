4a48fe9608aef6ca6999aa3a3baaeb5f6faebe2e
tanutanu918 <sutesutesute114117@gmail.com>
2020-11-03 21:59:28 +0900

    UI_branch_start

--- src/Page1.tsx
+++ src/Page1.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Page from "./component/Page";
+
 
 const Page1: React.FC = () => {
     return (
-        <span>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/A_2014/page2">Page2</Link>
-                    </li>
-                </ul>
-            </nav>
-            <div>Page1</div>
-        </span >
+        <div>
+            <span>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/A_2014/page2">Page2</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div>Page1</div>
+            </span >
+            <Page />
+        </div>
     )
 }
 

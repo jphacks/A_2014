b299087f4bcdf402ca94a41ba1d3f44178fc2c75
Endohiromu <aendous7@gmail.com>
2020-11-05 22:32:48 +0900

    committing page4

--- src/Page4.js
+++ src/Page4.js
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 import Button from "./component/Button";
 import Img from "./component/Img";
 import "./Page4.scss";
+import TwShare from "./component/Share";
+import logo from "./assets/jap.jpg";
 
-const Page4: React.FC = () => {
+const Page4 = () => {
     return (
+    <div>
         <span>
             <nav>
                 <ul>
@@ -16,6 +19,23 @@ const Page4: React.FC = () => {
             </nav>
             <div>Page4</div>
         </span >
+        <div className="container">
+            <div className="result">
+                <h1>"今日の目的地"</h1>
+                <h2>"結果を埋め込みたい"</h2>
+                <div className="area-text">
+                    <p>"aaa"</p>
+                </div>        
+                <div className="Gmap">
+                    <script>
+                    </script>
+                </div>
+            </div>
+          <img alt="pic" src={logo}/>
+          <TwShare className="Tw_share"/>
+
+        </div> 
+    </div>
     )
 }
 
--- src/component/Img.js
+++ src/component/Img.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Img.scss';
 
 class Img extends React.Component{
+  
   render() {
     return (
       <div>
-        <img className='main-img' alt={this.props.alt} src={this.props.src}></img>
+        <img className='main-img' alt={this.props.alt} src={this.props.src}/>
       </div>
     );
   }

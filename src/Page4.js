5ed96a93a474d50070d2c7c7f5e431e3258d1c81
Fironn <shihomihashi@gmail.com>
2020-11-07 02:52:21 +0900

    [ADD]location variation

--- src/Page4.js
+++ src/Page4.js
@@ -39,7 +39,7 @@ const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 
 const Page4 = (props) => {
     const [city, setCity] = useState("");
-    const [prefCode, setPrefCode] = useState(0);
+    const [prefCode, setPrefCode] = useState(-1);
     const [location, setLocation] = useState("");
     const url = "https://opendata.resas-portal.go.jp/"
     // add key
@@ -48,7 +48,7 @@ const Page4 = (props) => {
     const classes = useStyles();
 
     useEffect(() => {
-        if (city === "" && prefCode === 0) getData(url, key);
+        if (city === "" && prefCode === -1) getData(url, key);
     })
 
     const result = () => {
@@ -85,15 +85,60 @@ const Page4 = (props) => {
     const plus = (City, code) => {
         //prefをとりだすよ
         const prefList = {
-            1: "北海道",
-            2: "青森"
+            0: "北海道",
+            1: "青森県",
+            2: "岩手県",
+            3: "宮城県",
+            4: "秋田県",
+            5: "山形県",
+            6: "福島県",
+            7: "茨城県",
+            8: "栃木県",
+            9: "群馬県",
+            10: "埼玉県",
+            11: "千葉県",
+            12: "東京都",
+            13: "神奈川県",
+            14: "新潟県",
+            15: "富山県",
+            16: "石川県",
+            17: "福井県",
+            18: "山梨県",
+            19: "長野県",
+            20: "岐阜県",
+            21: "静岡県",
+            22: "愛知県",
+            23: "三重県",
+            24: "滋賀県",
+            25: "京都府",
+            26: "大阪府",
+            27: "兵庫県",
+            28: "奈良県",
+            29: "和歌山県",
+            30: "鳥取県",
+            31: "島根県",
+            32: "岡山県",
+            33: "広島県",
+            34: "山口県",
+            35: "徳島県",
+            36: "香川県",
+            37: "愛媛県",
+            38: "高知県",
+            39: "福岡県",
+            40: "佐賀県",
+            41: "長崎県",
+            42: "熊本県",
+            43: "大分県",
+            44: "宮崎県",
+            45: "鹿児島県",
+            46: "沖縄県"
         };
 
-        var Pref = prefList[1];
-        var ADDR = String(Pref + City);
+        // var Pref = prefList[1];
+        // var ADDR = String(Pref + City);
 
-        // var Pref = prefList[code] ? prefList[code] : prefList[1];
-        // var ADDR = prefList[code] ? String(Pref + City) : "北海道札幌市";
+        var Pref = prefList[code - 1] ? prefList[code - 1] : prefList[1];
+        var ADDR = prefList[code - 1] ? String(Pref + City) : "北海道札幌市";
 
         return ADDR
     }
@@ -106,38 +151,41 @@ const Page4 = (props) => {
                 ><ArrowBackRoundedIcon fontSize="large" style={{ color: "gray" }} /></Link>
             </IconButton>
             <Grid container className={classes.gridRoot}>
-                <Grid item xs={12} className={classes.grid} style={{ fontWeight: 600, fontSize: '1em' }}>
-                    今日の目的地
+                {city !== "" && prefCode !== -1 && location !== "" ? <>
+                    <Grid item xs={12} className={classes.grid} style={{ fontWeight: 600, fontSize: '1em' }}>
+                        今日の目的地
                 </Grid>
-                <Grid item xs={12} className={classes.grid}>
-                    <Card className={classes.root}>
-                        <CardHeader
-                            className="result-title"
-                            title={location}
-                            subheader={location}
-                        />
-                        <CardMedia
-                            className={classes.media}
-                            image={logo}
-                            title="pic"
-                        />
-                        <CardContent>
-                            <Typography className="area-text" variant="body2" color="textSecondary" component="p">
-                                景色がきれいな{location}の町です
+                    <Grid item xs={12} className={classes.grid}>
+                        <Card className={classes.root}>
+                            <CardHeader
+                                className="result-title"
+                                title={location}
+                                subheader={location}
+                            />
+                            <CardMedia
+                                className={classes.media}
+                                image={logo}
+                                title="pic"
+                            />
+                            <CardContent>
+                                <Typography className="area-text" variant="body2" color="textSecondary" component="p">
+                                    景色がきれいな{location}の町です
                             </Typography>
-                        </CardContent>
-                        <CardActions style={{ justifyContent: 'center' }} disableSpacing>
-                            <IconButton >
-                                <TwShare location={location} />
-                            </IconButton>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} className={classes.grid}>
-                    <Card className={classes.root} >
-                        <Map location={location} />
-                    </Card>
-                </Grid>
+                            </CardContent>
+                            <CardActions style={{ justifyContent: 'center' }} disableSpacing>
+                                <IconButton >
+                                    <TwShare location={location} />
+                                </IconButton>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} className={classes.grid}>
+                        <Card className={classes.root} >
+                            <Map location={location} />
+                        </Card>
+                    </Grid></>
+                    : <></>
+                }
             </Grid>
         </span>
     )
--- src/component/Share.js
+++ src/component/Share.js
@@ -11,7 +11,7 @@ const Tw_share = (props) => {
   return (
     <div id="share_button">
       {props.location ?
-        <TwitterShareButton url={["https://jphacks.github.io/A_2014/"]} title={"『ツルのひと押し』アプリで旅の行き先が決まったよ!\n\n「" + props.location + "」\n\n#ツルひと #JPHACKS2020\n"}>
+        <TwitterShareButton url={["https://jphacks.github.io/A_2014/"]} title={"『ツルのひと押し』アプリで旅の行き先が決まったよ!\n\n「" + props.location + "」\n\n#ツルひと #JPHACKS #JPHACKS2020\n"}>
           <TwitterIcon size={32} round />
         </TwitterShareButton>
         :
